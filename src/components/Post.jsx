4b43dee59662d358deb6d84b27973cf3b93a98d9
Brigido Alejo <brigsalejo@gmail.com>
2024-03-05 15:38:05 +0800

    init

---
+++ src/components/Post.jsx
@@ -0,0 +1,13 @@
+import classes from './Post.module.css'
+
+function Post({ author, body }) {
+
+  return (
+    <div className={classes.post}>
+      <p className={classes.author}>{author}</p>
+      <p className={classes.text}>{body}</p>
+    </div>
+  );
+}
+
+export default Post;
