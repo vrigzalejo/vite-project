d6a425bc48d4ff1887ca967d00a22055a76ba8af
Brigido Alejo <brigsalejo@gmail.com>
2024-03-05 18:26:42 +0800

    Done on modal

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,14 +5,9 @@ import NewPost from "./NewPost"
 import classes from "./PostsList.module.css"
 import Modal from "./Modal"
 
-function PostsList() {
-    const [modalIsVisible, setModalIsVisible] = useState(true)
+function PostsList({ isPosting, onStopPosting }) {
     const [enteredBody, setEnteredBody] = useState("")
     const [enteredAuthor, setEnteredAuthor] = useState("")
-
-    function hideModalHandler() {
-      setModalIsVisible(false)
-    }
 
     function bodyChangeHandler(event) {
       setEnteredBody(event.target.value)
@@ -24,12 +19,14 @@ function PostsList() {
 
     return (
       <>
-        {modalIsVisible && (<Modal onClose={hideModalHandler}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
-        </Modal>)}
+        {isPosting && (
+          <Modal onClose={onStopPosting}>
+            <NewPost
+              onBodyChange={bodyChangeHandler}
+              onAuthorChange={authorChangeHandler}
+            />
+          </Modal>
+        )}
         <ul className={classes.posts}>
           <Post author={enteredAuthor} body={enteredBody} />
           <Post author="test2" body="testtest2" />
