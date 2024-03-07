ac6f8e24bd03adac3ddb6ec17e3fa785680c769c
Brigido Alejo <brigsalejo@gmail.com>
2024-03-07 17:12:43 +0800

    post is now dynamic

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,18 +6,31 @@ import classes from "./PostsList.module.css"
 import Modal from "./Modal"
 
 function PostsList({ isPosting, onStopPosting }) {
+    const [posts, setPosts] = useState([])
+
+    function addPostHandler(postData) {
+      setPosts((existingPosts) => [postData, ...existingPosts])
+    }
+
     return (
       <>
         {isPosting && (
           <Modal onClose={onStopPosting}>
             <NewPost
               onCancel={onStopPosting}
+              onAddPost={addPostHandler}
             />
           </Modal>
         )}
-        <ul className={classes.posts}>
-          <Post author="test2" body="testtest2" />
-        </ul>
+        {posts.length > 0 && (<ul className={classes.posts}>
+          {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
+        </ul>)}
+        {posts.length === 0 && (
+          <div style={{textAlign: "center", color: "white"}}>
+            <h2>There are no posts yet.</h2>
+            <p>Start adding some!</p>
+          </div>
+        )}
       </>
     );
 }
