dbd6850b5b3896c80531dc154e77f0ac42df4042
Brigido Alejo <brigsalejo@gmail.com>
2024-03-07 15:09:26 +0800

    added cancel and submit

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,29 +6,16 @@ import classes from "./PostsList.module.css"
 import Modal from "./Modal"
 
 function PostsList({ isPosting, onStopPosting }) {
-    const [enteredBody, setEnteredBody] = useState("")
-    const [enteredAuthor, setEnteredAuthor] = useState("")
-
-    function bodyChangeHandler(event) {
-      setEnteredBody(event.target.value)
-    }
-
-    function authorChangeHandler(event) {
-      setEnteredAuthor(event.target.value);
-    }
-
     return (
       <>
         {isPosting && (
           <Modal onClose={onStopPosting}>
             <NewPost
-              onBodyChange={bodyChangeHandler}
-              onAuthorChange={authorChangeHandler}
+              onCancel={onStopPosting}
             />
           </Modal>
         )}
         <ul className={classes.posts}>
-          <Post author={enteredAuthor} body={enteredBody} />
           <Post author="test2" body="testtest2" />
         </ul>
       </>
